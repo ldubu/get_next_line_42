ef229f3ba15a25a420477c13457516de8389ea4c
Laura DUBUCHE <ldubuche@e1r12p1.42.fr>
2021-12-17 17:18:11 +0100

    travail du 17/12

--- get_next_line.c
+++ get_next_line.c
@@ -2,32 +2,40 @@
 
 char    *get_next_line(int fd) //cas où fd = 0, >0, renvoyer une erreur si fd correspond à rien où si read fail
 {
-    char *first_read;
+	char	*first_read;
+	char	*final_str;
+	size_t	len;
 
-    errno = 0;
-    first_read = (char *) malloc(sizeof(char) *24);
-    if (!first_read)
-    {
-        write(1, "error malloc", 12);
-        return (NULL);
-    }
-    if (read(fd, first_read, 24) <= 0)
-    {
-        if (errno ==  EISDIR)
-            write(1, "read error", 10);
-        return (NULL);
-    }
-    return (first_read);
+	errno = 0;
+	first_read = (char *) malloc(sizeof(char) *24);
+	if (!first_read)
+	{
+		write(1, "error malloc", 12);
+		return (NULL);
+	}
+	len = read(fd, first_read, 24);
+	if ( len <= 0)
+	{
+		write(1, "error read", 10);
+		return (NULL);
+	}
+	first_read[len] = '\0';
+	while (!ft_strrchr(first_read, '\n'))
+	{
+		
+	}
+
+	return (first_read);
 }
 
 int main(void)
 {
-    int     fd = open("test.txt", O_RDONLY);
-    char    *line;
-    if (fd <= 0)
-        write(1, "fail to open file", 17);
-    printf("fd = %d\n", fd);
-    line = get_next_line(fd);
-    printf("%s", line);
-    return (0);
+	int	 fd = open("test.txt", O_RDONLY);
+	char	*line;
+	if (fd <= 0)
+	write(1, "fail to open file", 17);
+	printf("fd = %d\n", fd);
+	line = get_next_line(fd);
+	printf("%s", line);
+	return (0);
 }
